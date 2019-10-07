0ffbb379d08f3dd33995109234c8ab49af95b157
EliSparow <max.pp@hotmail.fr>
2019-10-07 22:12:27 +0200

    added comment for Delete

--- routes/cat.route.js
+++ routes/cat.route.js
@@ -7,5 +7,6 @@ router.post('/', CatController.create);
 router.get('/', CatController.listCats);
 router.get('/:id', CatController.showCat);
 router.put('/:id', CatController.updateCat);
+// Did do delete route on purpose
 
 module.exports = router;
