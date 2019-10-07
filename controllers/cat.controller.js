98e87dcee99f3d7ecac6e848b71448d1927ff365
EliSparow <max.pp@hotmail.fr>
2019-10-07 20:50:28 +0200

    Created showCat Method

--- controllers/cat.controller.js
+++ controllers/cat.controller.js
@@ -5,7 +5,7 @@ const Cat = require('../models/cat.model');
     @methode POST
     @route /cat/
 */
-exports.create = async (req,res) => {
+exports.create = async (req, res) => {
     try {
         const { id, imageUrl } = req.body;
         const existingCat = await Cat.findOne({imageUrl: imageUrl});
@@ -39,14 +39,44 @@ exports.create = async (req,res) => {
     @methode Get
     @route /cat/
 */
-exports.listCats = async (req,res) => {
+exports.listCats = async (req, res) => {
     try {
         const cats = await Cat.find()
         
-        if(cats.length > 0) res.status(200).json(cats)
+        if(cats.length > 0) {
+            res.status(200).json(cats)
+        } else {
+            return res.status(404).json({
+                msg: "No cats found."
+            })
+        }
         
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Erreur serveur');
     }
 }
+
+/*
+    @desc Get a cat by id
+    @methode Get
+    @route /cat/:id
+*/
+exports.showCat = async (req, res) => {
+    try {
+        const cat = await Cat.findById(req.params.id)
+        
+        if(cat) {
+            res.status(200).json(cat)
+        } else {
+            return res.status(404).json({
+                msg: "No cat found."
+            })
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send('Erreur serveur');
+    }
+}
+
--- routes/cat.route.js
+++ routes/cat.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const CatController = require('../controllers/cat.controller');
 
 router.post('/', CatController.create);
-router.get('/', CatController.listCats)
+router.get('/', CatController.listCats);
+router.get('/:id', CatController.showCat)
 
 module.exports = router;
