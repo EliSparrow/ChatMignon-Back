a42c15b5c0d41677a44b9e0bba92d47e704736d4
EliSparow <max.pp@hotmail.fr>
2019-10-17 15:19:31 +0200

    edited CORS to allow all origins

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({extended: false}));
 
-var allowedOrigins = ['http://localhost:3000'];
+var allowedOrigins = ['*'];
 app.use(cors({
     origin: function(origin, callback) {
         if (!origin)
